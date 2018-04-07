6c131ad668a82cdf42c90bd056af7d7242bd54bb
Felipe Henrique <felipehfsouza@gmail.com>
2018-04-07 16:00:48 -0300

    Remoção de random.go e organização em pastas

--- ImageManipulation/image.go
+++ ImageManipulation/image.go
@@ -11,8 +11,9 @@ func main() {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 50))
 
 	// Draw a red dot at (2, 3)
-	img.Set(2, 3, color.RGBA{255, 0, 0, 255})
-
+	for y := 3; i < 36; y++{
+		img.Set(2, y, color.RGBA{255, 0, 0, 255})
+	}
 	// Save to out.png
 	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
 	defer f.Close()
--- analise.go
+++ analise.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-func analise(a1, r, n int) int {
+func progressaoAritmetica(a1, r, n int) int {
 	return a1 + (n - 1) * r
 }
 
@@ -15,6 +15,5 @@ func jurosCompostos(c, i, n float64) float64 {
 
 func main(){
 	montante := jurosCompostos(2000.00, 0.043, 10)
-
 	fmt.Println(montante)
 }
